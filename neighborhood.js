0724f4a152e1fe2178d383e8441590d592436934
Heet Barot <heerbarot21@gmail.com>
2021-10-31 20:19:05 -0400

    Addition implementation working

---
+++ neighborhood.js
@@ -0,0 +1,59 @@
+let thai = document.querySelector('#Thai');
+thai.addEventListener('click', thaiRes);
+
+function thaiRes(evt) {
+	evt.preventDefault();
+	
+	//console.log('form submit');
+	alert("Black");
+}
+
+let mexican = document.querySelector('#Mexican');
+mexican.addEventListener('click', mexicanRes);
+
+function mexicanRes(evt) {
+	evt.preventDefault();
+	
+	//console.log('form submit');
+	alert("Leh-Ladakh");
+}
+
+let american = document.querySelector('#American');
+american.addEventListener('click', americanRes);
+
+function americanRes(evt) {
+	evt.preventDefault();
+	
+	//console.log('form submit');
+	alert("Hindu");
+}
+
+let indian = document.querySelector('#Indian');
+indian.addEventListener('click', indianRes);
+
+function indianRes(evt) {
+	evt.preventDefault();
+	
+	//console.log('form submit');
+	alert("Hindu");
+}
+
+let italian = document.querySelector('#Italian');
+italian.addEventListener('click', italianRes);
+
+function italianRes(evt) {
+	evt.preventDefault();
+	
+	//console.log('form submit');
+	alert("Hindu");
+}
+
+let japanese = document.querySelector('#Japanese');
+japanese.addEventListener('click', japaneseRes);
+
+function japaneseRes(evt) {
+	evt.preventDefault();
+	
+	//console.log('form submit');
+	alert("Hindu");
+}
